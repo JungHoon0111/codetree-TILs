fdf7e1757908ea6c5d4d048e1c984a0fa8cbb61f
JungHoon0111 <162983253+JungHoon0111@users.noreply.github.com>
2024-07-17 19:16:13 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240717/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int n, int m){
-    int x,y;
-    if(m>n) {
-        x = n;
-        y = m;
+int find(int n, int m){
+    int k = 1;
+    while(true){
+        if(k%n==0 && k%m==0){
+            return k;
+        }
+        k++;
     }
-    else {
-        x = m;
-        y = n;
-    }
-    while(y!=0){
-        int temp = x;
-        x = y;
-        y = y % temp;
-    }
-
-    return x;
 }
 
 int main(){
     int n,m;
     cin>>n>>m;
-    cout<< (n*m) / gcd(n,m);
+    cout<<find(n,m);
 }
